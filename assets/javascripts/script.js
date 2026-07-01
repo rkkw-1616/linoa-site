bd3a70ab0f4f1be8b93989862b0a6ffb273dd8ae
Rikako Watanabe <Rikako.w.1616@gmail.com>
2026-07-01 22:30:49 +0900

    　7/1作成分

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector(".c-hamburgerButton");
-  const globalNav = document.querySelector(".p-topContents__bottom");
+  const globalNav = document.querySelector(".p-header__nav");
   if (!hamburgerButton || !globalNav) return;
-
   hamburgerButton.addEventListener("click", () => {
     const isOpen = hamburgerButton.classList.toggle("is-open");
     globalNav.classList.toggle("is-open");
-
     hamburgerButton.setAttribute("aria-expanded", String(isOpen));
     hamburgerButton.setAttribute(
       "aria-label",
